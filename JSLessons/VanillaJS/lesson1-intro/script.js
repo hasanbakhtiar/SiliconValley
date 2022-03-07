e9f216bab49c6cc5bef8c615e6efe71370db28ec
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-03-07 12:04:46 +0400

    js intro

---
+++ JSLessons/VanillaJS/lesson1-intro/script.js
@@ -0,0 +1,25 @@
+const myBtn = document.querySelector('button');
+const myText = document.querySelector('p');
+
+const seeCapital =()=>{
+    myText.innerHTML = "Baku";
+    myText.style.background = "red";
+    myText.style.width = "100px";
+}
+
+myBtn.addEventListener('click', seeCapital);
+
+
+
+
+// document.write("Hello1");
+
+
+// debugger;
+// console.log("Hello1");
+// console.log("Hello2");
+// console.log("Hello3");
+// console.loga("Hello4");
+// console.log("Hello5");
+// console.log("Hello6");
+// console.log("Hello7");
