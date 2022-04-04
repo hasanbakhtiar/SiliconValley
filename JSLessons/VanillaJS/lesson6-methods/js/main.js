3741873736c4579ade47503e248e3e4dd877721d
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-04-04 11:40:05 +0400

    methods

---
+++ JSLessons/VanillaJS/lesson6-methods/js/main.js
@@ -0,0 +1,37 @@
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+
+// // delete fruits[1];
+// fruits.push("Hello")
+// console.log(fruits);
+// // document.write(fruits);
+
+
+
+// const myArr = ["Banan","Orange","Apple","Strawberry"];
+// for(let key in myArr){
+    
+//     document.write(myArr[key]+"<br>")
+// }
+// // document.write(myArr);
+
+
+// const addList = ()=>{
+//     myArr.shift();
+//     for(let key in myArr){
+    
+//         document.write(myArr[key]+"<br>")
+//     }
+
+    
+// }
+
+// document.querySelector('button').addEventListener('click', addList)
+
+
+for (let index = 0; index < 100; index++) {
+    if (index > 20 && index < 30) {
+        continue;
+    }
+    console.log(index);
+}
