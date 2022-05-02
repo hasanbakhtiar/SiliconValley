6bb88e7a071518b403d7819ebf7213cb430cad29
HAsan Bakhtiar <hasan.doctype@gmail.com>
2022-05-02 11:53:27 +0400

    BOM enter

---
+++ JSLessons/VanillaJS/lesson12-BOMenter/js/main.js
@@ -0,0 +1,59 @@
+// document.querySelector('span').innerHTML = `${window.innerWidth}px`;
+// document.querySelector('span').innerHTML = `${window.innerHeight}px`;
+// document.querySelector('span').innerHTML = `${screen.width}px`;
+// if (screen.width == 1536) {
+//     document.querySelector('.box').style.background="#333";
+// }else if(screen.width >= 2000){
+//     document.querySelector('.box').style.background="#433";
+
+// }
+
+// document.querySelector('span').innerHTML = `${screen.height}px`;
+// document.querySelector('span').innerHTML = `${screen.availHeight}px`;
+// document.querySelector('span').innerHTML = `${screen.colorDepth}`;
+// document.querySelector('span').innerHTML = `${screen.pixelDepth}`;
+// document.querySelector('span').innerHTML = `${window.location}`;
+// document.querySelector('span').innerHTML = `${window.location.pathname}`;
+// document.querySelector('span').innerHTML = `${window.location.hostname}`;
+// document.querySelector('span').innerHTML = `${window.location.href}`;
+// document.querySelector('span').innerHTML = `${window.location.protocol}`;
+
+// if (window.location.protocol == 'http') {
+//     console.log('not secure');
+// }else if(window.location.protocol == 'https'){
+//     console.log('secure');
+// }else{
+//     console.log('not secure');
+// }
+
+// document.querySelector('span').innerHTML = `${window.location.protocol}`;
+
+
+// document.querySelector('button').onclick = ()=>{
+//     if (document.querySelector('button').innerHTML == "next") {
+//     history.forward()
+        
+//     }
+// }
+// let a;
+// let myWindow;
+// document.querySelectorAll('button')[0].onclick = ()=>{
+//     // a =  window.open('https://google.com', "google", "width=400, height=200");
+//     myWindow = window.open("home.htm", "", "width=200, height=100");
+// }
+
+// document.querySelectorAll('button')[1].onclick = ()=>{
+//     // a.close();
+//     // a.resizeTo(500, 500);
+//     // myWindow.resizeTo(1000, 1000);
+// }
+
+
+// document.querySelector('span').innerHTML = `${navigator.appCodeName}`;
+// document.querySelector('span').innerHTML = `${navigator.appName}`;
+// document.querySelector('span').innerHTML = `${navigator.appName}`;
+// document.querySelector('span').innerHTML = `${navigator.product}`;
+// document.querySelector('span').innerHTML = `${navigator.appVersion}`;
+// document.querySelector('span').innerHTML = `${navigator.appVersion}`;
+document.querySelector('span').innerHTML = `${navigator.language}`;
+// http://www.lingoes.net/en/translator/langcode.htm
