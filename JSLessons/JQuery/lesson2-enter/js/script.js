1c6f8490d9f90638839f66ac5d20adc8f789125e
HAsan Bakhtiar <hasan.doctype@gmail.com>
2022-05-06 15:22:23 +0400

    jquery enter

---
+++ JSLessons/JQuery/lesson2-enter/js/script.js
@@ -0,0 +1,25 @@
+// $(function(){
+//         $('ne ile').nece(function() {
+//            $(neyi).etsin; 
+//         })  ;
+// });
+
+// $(document).ready(function(){
+//     $('button').click(function(){
+//         $('p').toggle(1000);
+//     })
+// });
+$(document).ready(function(){
+    $('button').click(function(){
+        // $('.a').hide(1000);
+        // $('.a').show(1000);
+        // $('.a').toggle(1000);
+        // $('#a').css('color','red');
+        // $('#a').css({'color':'red','background':"black"});
+        // $('#a').addClass('b');
+        // $('#a').removeClass('b');
+        $('#a').toggleClass('b');
+        // $(this).hide();
+
+    })
+});
---
+++ JSLessons/VanillaJS/lesson13-TimeFunc/js/script.js
@@ -0,0 +1,60 @@
+let a=10.951;
+
+// document.querySelector('p').innerHTML = a.toExponential(2);
+// document.querySelector('p').innerHTML = a.toFixed(1);
+// document.querySelector('p').innerHTML = a.toPrecision(2);
+// document.querySelector('p').innerHTML = a.valueOf();
+// document.querySelector('p').innerHTML = parseInt(a);
+// document.querySelector('p').innerHTML = parseFloat(a);
+// document.querySelector('p').innerHTML = Number.MAX_VALUE;
+// document.querySelector('p').innerHTML = Number.MIN_VALUE;
+// document.querySelector('p').innerHTML = Math.floor(Math.random()*10)+1;
+
+
+
+
+
+
+
+// setInterval(function,time);
+
+// const myTime = () => {
+  // document.querySelector('p').innerHTML = new Date();
+  // alert('hello');
+  // document.write('hello ');
+//   let a = [];
+
+//     a.push("hello");
+
+
+//   for (x in a) {
+    // document.write(a);
+//   }
+//   console.log(a);
+// };
+
+// 1000ms = 1s
+// setInterval(myTime,1000);
+
+
+
+
+
+// const count=()=>{
+//     // document.write('Hello ');
+//     // document.querySelector('p').innerHTML = "Hello";
+//     console.log('hello');
+// }
+
+// let a =  setInterval(count,1000);
+// document.querySelector('button').addEventListener('click',()=>{
+//     clearInterval(a)
+// });
+
+
+
+    // setTimeout(() => {
+    //     alert('hello');
+    // }, 2000);
+
+    // document.write(new Date().getFullYear());
